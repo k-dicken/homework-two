58ec7198852bc97762da2a589dd28b83ae7a3547
k-dicken <78804757+k-dicken@users.noreply.github.com>
2022-09-17 23:51:45 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,15 @@
+import * as MODEL from "./model.js"
+
+function initListeners() {
+    $("nav a").click((e) => {
+        e.preventDefault();
+        let btnID = e.currentTarget.id;
+
+        MODEL.setCurrentPageContent(btnID);
+    })
+}
+
+$(document).ready(function () {
+    initListeners();
+});
+
---
+++ app/model.js
@@ -0,0 +1,170 @@
+//page content
+var homeContent = `<div id="homeContent" class="page">
+<div class="home-hero image">
+  <h2>IU PROVIDES A WORLD CLASS</h2>
+  <h1>EDUCATION</h1>
+</div>  
+<div class="home-values">
+  <h1>Our Values</h1>
+  <br>
+  <div class="values-row">
+    <div class="values-links">
+      <a id="diverityEquityInclusion" href="#">DIVERSITY, EQUITY & INCLUSION</a>
+      <a id="studentSuccess" href="#">STUDENT SUCCESS</a>
+      <a id="educationalExcellence" href="#">EDUCATIONAL EXCELLENCE</a>
+      <a id="communityEngagment" href="#">COMMUNITY ENGAGEMENT</a>
+      <a id="safety" href="#">SAFETY</a>
+    </div>
+    <div class="values-content">
+      <h2>DIVERSITY, EQUITY & INCLUSION</h3>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Laborum exercitationem qui vitae corrupti, vero, explicabo ullam at sit excepturi doloribus adipisci odio velit cum sapiente, harum illum quis facilis non.</p>
+      <button>Our Strategic Plan</button>
+    </div>
+  </div>
+</div>  
+</div>`;
+var campusesContent = `<div id="campusesContent" class="page">
+<div class="campuses-map image">
+  <h1>OUR CAMPUSES</h1>
+</div>
+<div class="campuses-links">
+  <a href="">IU Bloomington</a>
+  <p>|</p>
+  <a href="">IU Kokomo</a>
+  <p>|</p>
+  <a href="">IU Fort Wayne</a>  
+  <p>|</p>
+  <a href="">IU South Bend</a>
+  <p>|</p>
+  <a href="">IUPUI</a>  
+  <p>|</p>
+  <a href="">IUPUC</a>
+</div>
+<div class="campuses-more">
+  <h1>Learn More About Our Cities</h1>
+  <div class="campuses-buttons">
+    <a href="#">Internship & Career Opportuities</a>
+    <a href="#">Fun Things to Do</a>
+  </div>
+</div>
+</div>`;
+var degreesContent = `<div id="degreesContent" class="page">
+<div class="degrees-desc">
+  <h1>Explore degrees and majors</h1>
+  <h2>Choose the program that's right for you</h2>
+  <p>We believe in giving students the power of choice by offering a wide variety of world-class IU and Purdue programs. IUPUI has more than 550 undergraduate, graduate, and professional programs for you to choose from, with classes offered on campus, online, or both.
+  <br>
+  Explore nationally-ranked health sciences programs, take classes from the world’s first school of philanthropy, and find unique programs only offered at IUPUI, such as the United States’s only motorsports engineering degree.</p>
+</div>
+<div class="degrees-links">
+  <h2>Discover the degree for you</h2>
+  <br>
+  <div class="link">
+    <a href="#">
+      <span>CRT</span>
+      Academic Advising—Online</a>
+      <p>School of Education</p>
+  </div>
+  <div class="link">
+    <a href="#">
+      <span>B.S.B.</span>
+      Accounting</a>
+    <p>Kelley School of Business</p>
+  </div>
+  <div class="link">
+    <a href="#">
+      <span>CRT</span>
+      Accounting</a>
+    <p>Kelley School of Business</p>
+  </div>
+  <div class="link">
+    <a href="#">
+      <span>M.B.A</span>
+      Accounting</a>
+    <p>Kelley School of Business</p>
+  </div>
+  <div class="link">
+    <a href="#">
+      <span>M.S.A</span>
+      Accounting</a>
+    <p>Kelley School of Business</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>M.S.A.</span>Accounting/Business Administration</a>
+    <p>Kelley School of Business</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>Ph.D</span>Addiction Neuroscience</a>
+    <p>School of Nursing</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>M.S.N.</span>Adult-Gerontology Acute Care Nurse Practitioner</a>
+    <p>School of Nursing</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>P.M.C.</span>Adult-Gerontology Acute Care Nurse Practitioner</a>
+    <p>School of Nursing</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>M.S.N.</span>Adult-Gerontology Clinical Nurse Specialist</a>
+    <p>School of Nursing</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>P.M.C.</span>Adult-Gerontology Clinical Nurse Specialist</a>
+    <p>School of Nursing</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>M.S.N.</span>Adult-Gerontology Primary Care Nurse Practitioner</a>
+    <p>School of Nursing</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>P.M.C.</span>Adult-Gerontology Primary Care Nurse Practitioner</a>
+    <p>School of Nursing</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>CRT</span>Advocacy Skills</a>
+    <p>Robert H. McKinney School of Law</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>CRT</span>African Studies</a>
+    <p>School of Liberal Arts</p>
+  </div>
+  <div class="link">
+    <a href="#"><span>B.A.</span>Africana Studies</a>
+    <p>School of Liberal Arts</p>
+  </div>
+</div>
+</div>`;
+var applyContent = `<div id="applyContent" class="page">
+<div class="apply-image image"></div>
+
+<div class="apply-desc">
+  <h2>Apply for admission to IUPUI</h2>
+  <p>You’ve decided that IUPUI is the right school for you, and you’re ready to apply. To get started, read the descriptions of the types of applicants on this page and determine which category you fit into. Then visit the page for your applicant type to learn about admission standards, the application process, deadlines, and other important information.</p>
+</div>
+
+<div class="apply-forms">
+  <div class="form-link">
+    <div class="freshmanapply-image image"></div>
+    <h2>Freshman Applicant</h2>
+    <span>JOIN THE CLASS OF 2027</span>
+    <p>Learn how to apply for admission to IUPUI if you are currently a high school student, or have graduated from high school and have not enrolled in a degree-seeking program at a college or university.</p>
+    <a href="#">Learn how to apply for freshman admission</a>
+  </div>
+<div div class="form-link">
+    <div class="adultapply-image image"></div>
+    <h2>Transfer & Adult Applicant</h2>
+    <span>MAKE THE MOVE TO IU</span>
+    <p>Learn how to apply if you've previously enrolled in a college degree program (including programs at another IU campus), are an adult learner looking to start something new, or just want to take a class as a non-degree student.</p>
+    <a href="#">Learn how to apply as a transfer or adult student</a>
+</div>
+</div>
+</div>`;
+
+function setCurrentPageContent(pageID) {
+    let contentName = pageID + "Content";
+    $("#app").html(eval(contentName));
+    console.log(contentName);
+}
+
+export { setCurrentPageContent };
